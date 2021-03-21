597c39e15a7c1ea912fe95a0a214c8dedac851ab
Genis Bardales <genis96@yahoo.com>
2021-03-21 16:47:19 -0400

    working on the service workers js

---
+++ public/service-worker.js
@@ -0,0 +1,15 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/indexDb.js",
+    "/manifest.webmanifest",
+    "/service-worker.js",
+    "/style.css"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+
+
